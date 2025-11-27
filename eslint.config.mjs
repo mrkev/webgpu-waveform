d8f532e2d518e9f1b63bdacd1e107f660272202a
Kevin Chavez <mr.kev@me.com>
2025-11-27 17:39:35 -0600

    modernize esling config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,82 +1,58 @@
-import { fixupConfigRules } from "@eslint/compat";
-import { FlatCompat } from "@eslint/eslintrc";
-import eslint from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import tseslint from "typescript-eslint";
+import js from "@eslint/js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: eslint.configs.recommended,
-  allConfig: eslint.configs.all,
-});
-
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+export default defineConfig([
   {
     ignores: ["**/dist", "eslint.config.mjs", "docs"],
   },
-  ...fixupConfigRules(
-    compat.extends(
-      "plugin:react-hooks/recommended",
-      "plugin:react/recommended",
-      "plugin:react/jsx-runtime"
-    )
-  ),
+
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  react.configs.flat.recommended,
+  reactHooks.configs.flat.recommended,
+
   {
-    plugins: {
-      "react-refresh": reactRefresh,
-    },
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-
+    files: ["**/*.{ts,tsx,js,jsx}"],
     settings: {
       react: {
         version: "detect",
       },
     },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        projectService: true, // Auto-discovers tsconfig.*.json files
+      },
+    },
 
     rules: {
-      "@typescript-eslint/no-empty-function": [
-        "warn",
-        {
-          allow: ["private-constructors"],
-        },
-      ],
-
-      "@typescript-eslint/ban-types": "off",
-      "no-useless-escape": "off",
-      indent: "off",
-      quotes: "off",
-      "no-unused-vars": "off",
-
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-          caughtErrorsIgnorePattern: "^_",
-        },
-      ],
-
+      "@typescript-eslint/no-unnecessary-type-constraint": "warn",
+      "@typescript-eslint/no-unused-vars": ["off", { argsIgnorePattern: "^_" }],
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-var-requires": "off",
-      "@typescript-eslint/ban-ts-ignore": "off",
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "react/no-unescaped-entities": "off",
-      "no-case-declarations": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-this-alias": "off",
+      "@typescript-eslint/no-unnecessary-type-constraint": "off",
+      "react/react-in-jsx-scope": "off", // unnecessary for modern React
+      "react/no-children-prop": "off",
       "react-refresh/only-export-components": [
         "warn",
         {
@@ -84,5 +60,5 @@ export default tseslint.config(
         },
       ],
     },
-  }
-);
+  },
+]);
